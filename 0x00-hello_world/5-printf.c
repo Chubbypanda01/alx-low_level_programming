14304571e519ed0ccfa37e1b2decd995c2120978
Abigail Ejeh <ejehabigail58@gmail.com>
2022-09-09 16:37:12 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,9 @@
 /**
  * Description: main - prints out a string to stdout.
  * Return: 0 if success.
- * */
-int main(void)
+ */
+int main (void)
 {
-puts("\"programming is like building a multilingual puzzle"):
-return (0):
-}	
+	printf("with proper grammar, but the outcome is a piece of art.\n");
+	return (0);
+}
