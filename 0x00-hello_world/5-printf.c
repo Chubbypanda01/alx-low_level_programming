9eec918d6cb1e8bcaf001f39dbf3ff1e63c82f27
Abigail Ejeh <ejehabigail58@gmail.com>
2022-09-11 14:24:19 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - the containing func
+ * main - The containing func
  * Return: 0 if successful
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
