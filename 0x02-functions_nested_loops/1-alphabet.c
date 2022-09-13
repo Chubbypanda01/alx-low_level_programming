081b5ca392348d0f86bb11c271ade867bdc41c69
Abigail Ejeh <ejehabigail58@gmail.com>
2022-09-13 01:29:16 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - print_alphabet
+ * Return: 0
+ */
+
+void print alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
