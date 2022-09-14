4e624e921c7187a9c02c8d91cbf0d63e620bc7a0
Abigail Ejeh <ejehabigail58@gmail.com>
2022-09-13 21:31:08 -0700

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,12 @@
 #include "main.h"
-
 /**
- * _islower - short description, single line
- * @c: contains value to be compared
+ * _islower - short description. single line
+ * @c: contains value ro be compared
  * Return: Always 0.
  */
+int _islower(int c)
 
 {
-
 	if (c > 'a' && c < 'z')
 	{
 		return (1);
