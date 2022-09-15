dbfecc8f33ab4f64b9e37614a09fd70c2cd883f7
Abigail Ejeh <ejehabigail58@gmail.com>
2022-09-15 14:45:27 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - check uppercase
+ * @c: parameter
+ * Return: 0 if successful
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c < 91)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
