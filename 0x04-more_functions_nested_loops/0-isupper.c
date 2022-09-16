f7b64d759b8877601adb30fbe5ee4c522650effd
Abigail Ejeh <ejehabigail58@gmail.com>
2022-09-15 17:57:26 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,19 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _isupper - checks uppercase
- * @c: checks imput function
- *
- * Return: returns1 if 'c' is uppercase
- * otherwise always 0 (success)
+ * @c: parameter
+ * Return: 0 if successful
  */
 
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
+	if (c >= 65 && c < 91)
+	{
 		return (1);
+	}
+	else
+	{
 		return (0);
+	}
 }
