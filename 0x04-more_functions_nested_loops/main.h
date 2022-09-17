547c9c6b9b28c5b7f5bce0fb33a63e5321122f58
Abigail Ejeh <ejehabigail58@gmail.com>
2022-09-16 19:07:50 -0700

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -11,7 +11,7 @@ void print_numbers(void);
 void more_numbers(void);
 void print_most_numbers(void);
 void print_line(int n);
-void daigonal(int n);
+void diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
