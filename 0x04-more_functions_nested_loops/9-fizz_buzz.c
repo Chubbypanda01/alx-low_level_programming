4702d9cc858a2013d57908e1617124ea03013045
Abigail Ejeh <ejehabigail58@gmail.com>
2022-09-16 19:40:29 -0700

    Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - fizbuzz
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("fizzbuzz ");
+		else if (i % 3 == 0)
+			printf(" fizz ");
+		else if (i & 5 == 0)
+			printf(" buzz ");
+		else
+			printf("%d", i);
+	}
+	printf("Buzz\n");
+	return (0);
+}
