a8214ff6d3d9d0a769f98ac0002b856d526311be
Abigail Ejeh <ejehabigail58@gmail.com>
2022-09-18 15:13:32 -0700

    Task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -4,11 +4,11 @@
  * main - print prime
  * Return: 0
  */
+
 int main(void)
 {
 	unsigned long int i = 3, n = 612852475143;
-
-	for (; i < 1207; i += 2)
+	for ( ; i < 12057; i += 2)
 	{
 		while (n % i == 0 && n != i)
 			n /= i;
