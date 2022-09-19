0845bdcd52176b28462eca9225de8ad62bf2f726
Abigail Ejeh <ejehabigail58@gmail.com>
2022-09-19 04:57:10 -0700

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,23 @@
+#include <stdio.h>
 #include "main.h"
+
 /**
- * _puts - puts to stdout
- * @str: params
+ * _puts - print a string
+ * @str: string to print
+ *
+ * Description: print a string
+ * On success: return no error
  */
 
 void _puts(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (*(str = i) != '\0')
 	{
-		_pytchar(str[i]);
+		_putchar(*(str = i));
 		i++;
 	}
 
-	_putchar('\n');
+	putchar(10);
 }
