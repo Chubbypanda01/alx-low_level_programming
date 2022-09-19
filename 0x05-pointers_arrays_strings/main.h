0d19fb406828bc4d3aba661d56fe1f939ae67fb9
Abigail Ejeh <ejehabigail58@gmail.com>
2022-09-19 04:30:15 -0700

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * void prototypes{}
+ * int prototypes{}
+ */
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -10,3 +18,5 @@ void puts_half(char *str);
 void print_arrays(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+
+#endif /*MAIN_H*/
