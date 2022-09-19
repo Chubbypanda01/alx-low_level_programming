99b20be4dbafb8051d37a0b8ee720f66038a0007
Abigail Ejeh <ejehabigail58@gmail.com>
2022-09-19 03:26:26 -0700

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - puts to stdout
+ * @str: params
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_pytchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
