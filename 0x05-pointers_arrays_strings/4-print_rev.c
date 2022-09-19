beab1d6ad1d552da3445cb2a5780620127fb655f
Abigail Ejeh <ejehabigail58@gmail.com>
2022-09-19 16:20:04 -0700

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,10 +9,17 @@
 void print_rev(char *s)
 {
 	int i = 0;
+	int j;
 
-	while (s[i])
+	while (s[i] != '\0')
+	{
 		i++;
-	while (i++)
-		_putchar(s[i]);
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
 	_putchar('\n');
 }
+
+
