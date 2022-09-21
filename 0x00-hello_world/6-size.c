d81bf6f469f0f1dbb3a0ad898ff6213aba18a64e
Abigail Ejeh <ejehabigail58@gmail.com>
2022-09-21 10:18:57 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - The containing func
- * Return: 0 if no error
+ * main - prints the size of various types
+ * Return: 0 if exited properly, non-zero otherwise
  */
+
 int main(void)
+
 {
-	printf("Size of a char: %d byte(s)\n", sizeof(char));
-	printf("Size of an int: %d byte(s)\n", sizeof(int));
-	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-	printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	printf("size of a char: %d byte(s)\n", sizeof(char));
+	printf("size of an int: %d byte(s)\n", sizeof(int));
+	printf("size of a long int: %d byte(s)\n", sizeof(long));
+	printf("size of a long long int: %d byte(s)\n", sizeof(long long));
+	printf("size of a float: %d byte(s)\n". sizeof(float));
 	return (0);
 }
