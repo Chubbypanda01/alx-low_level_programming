68a68a2d7a45e32a5c06f94d4d7f69fc900f88ba
Abigail Ejeh <ejehabigail58@gmail.com>
2022-09-23 14:30:25 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * cap_string - capitalizes every word of a string
+ * cap_string - capitalizes all words of a string
  * @s: input string.
  * Return: the pointer to the dest
  */
@@ -10,7 +10,7 @@ char *cap_string(char *s)
 	int count = 0, i;
 	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	if (*(s + count) >= 97 && *s(s + count) <= 122)
+	if (*(s + count) >= 97 && *(s + count) <= 122)
 		*(s + count) = *(s + count) - 32;
 	count++;
 	while (*(s + count) != '\0')
@@ -19,7 +19,7 @@ char *cap_string(char *s)
 		{
 			if (*(s + count) == sep_words[i])
 			{
-				if ((*(s + (count + 1) >= 97) && (*(s + count + 1)) <= 122))
+				if ((*(s + (count + 1)) >= 97) && (*(s + count + 1)) <= 122))
 					*(s + (count + 1)) = *(s + (count + 1)) - 32;
 				break;
 			}
